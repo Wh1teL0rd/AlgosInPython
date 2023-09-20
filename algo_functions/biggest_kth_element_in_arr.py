986c5dc8e9a4ca1a6a9303db41aeded7c8d33b7c
Wh1teL0rd <pacman.oleg@gmail.com>
2023-09-20 15:28:56 +0300

    fixed function and add unit-tests

--- algo_functions/biggest_kth_element_in_arr.py
+++ algo_functions/biggest_kth_element_in_arr.py
@@ -2,13 +2,10 @@ def biggest_kth_element_in_arr(nums, k):
     if k <= 0 or k > len(nums):
         return "Wrong k"
 
-    if len(nums) < k:
-        return "Length of array < k"
-
     sorted_nums = sorted(nums, reverse=True)
 
     kth_biggest = sorted_nums[k-1]
 
-    position = nums.index(sorted_nums[k-1])
+    position = nums.index(kth_biggest)
 
     return kth_biggest, position
---
+++ unit_tests/biggest_kth_test.py
@@ -0,0 +1,33 @@
+import unittest
+
+from algo_functions.biggest_kth_element_in_arr import biggest_kth_element_in_arr
+
+
+class TestBiggestKthInArr(unittest.TestCase):
+    def test_biggest_kth_element_in_arr(self):
+        nums = [15, 7, 22, 9, 36, 2, 42, 18]
+        k = 3
+        result = biggest_kth_element_in_arr(nums, k)
+        self.assertEqual(result, (22, 2))
+
+    def test_wrong_k(self):
+        nums = [4, 2, 7, 1, 9]
+        k = 0
+        result = biggest_kth_element_in_arr(nums, k)
+        self.assertEqual(result, "Wrong k")
+
+    def test_length_of_arr_less_than_k(self):
+        nums = [4, 2, 7, 1, 9]
+        k = 23
+        result = biggest_kth_element_in_arr(nums, k)
+        self.assertEqual(result, "Wrong k")
+
+    def test_empty_array(self):
+        nums = []
+        k = 1
+        result = biggest_kth_element_in_arr(nums, k)
+        self.assertEqual(result, "Wrong k")
+
+
+if __name__ == '__main__':
+    unittest.main()
